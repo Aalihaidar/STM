6d20ba037bfa14bd276eb9cc9c468aaa82fc2116
MaiAbboud <ma1991abb@gmail.com>
2022-07-31 15:10:03 +0300

    try 4 head instead of 1 head in concatenation block

--- models/concatenation/builder.py
+++ models/concatenation/builder.py
@@ -3,6 +3,6 @@ def build_concatenation(config: dict, with_multi_scale_wrapper=True):
     dim =  config['transformer']['backbone']['dim']
     # template_shape = config['transformer']['backbone']['template']['shape'] = [7, 7]
 
-    return MultiheadAttention(dim,1)
+    return MultiheadAttention(dim,4)
     # head_config = config['head']
     # head_type = head_config['type']
