a1b1e9690b79c073e110e855733e8974a7431cf2
MaiAbboud <ma1991abb@gmail.com>
2022-11-23 13:12:32 +0300

    fixe different device in track and evaluation mode

--- data/operator/bbox/spatial/vectorized/torch/intersection.py
+++ data/operator/bbox/spatial/vectorized/torch/intersection.py
@@ -2,6 +2,9 @@ import torch
 
 
 def bbox_compute_intersection_vectorized(bbox_a: torch.Tensor, bbox_b: torch.Tensor):
+    if bbox_b.get_device() != bbox_a.get_device():
+        device = bbox_b.get_device()
+        bbox_a = bbox_a.to(device)
     intersection = torch.cat((torch.maximum(bbox_a[..., :2], bbox_b[..., :2]), torch.minimum(bbox_a[..., 2:], bbox_b[..., 2:])), dim=-1)
     intersection[(intersection[..., 2] - intersection[..., 0]) <= 0, ...] = 0
     intersection[(intersection[..., 3] - intersection[..., 1]) <= 0, ...] = 0
